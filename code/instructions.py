d364d3db1cee7ac9a32c41c8481714d5b8ab7e7c
Rasmus <rasmus.v.makinen@aalto.fi>
2025-05-31 20:01:17 +0300

    Added more text to instructions

--- code/instructions.py
+++ code/instructions.py
@@ -1,3 +1,8 @@
 INSTRUCTIONS = "Everything has to be formated with the following specifications." \
                "Nothing can deviate from these instructions." \
+               "Everything must be in this format: tag, text." \
+               "Here are all the possible tags: Location, Date, Service personnel, Area of service, Time of service, Reason of service, Work description, Problems with service, Notes, Supplies and amounts, Weight, Length, Width." \
+               "There is also the tag 'special' which is only used if the user especially asks to add something that doesn't fit any of the other tags." \
+               "Keep all the text concise and only give the required information. Never ask follow up questions." \
+               "If there is a problem or the input given by the user is not enough, return only ERROR." \
                ""
