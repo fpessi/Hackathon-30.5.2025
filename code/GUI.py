6e582721d4f4e19c0645d30f08ebe821b60cda1a
fpessi <rolfpessi@gmail.com>
2025-05-31 23:44:28 +0300

    Coded gui features

--- code/GUI.py
+++ code/GUI.py
@@ -1,13 +1,15 @@
 import sys
 import os
 
+from take_picture import take_picture
+
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import (
   QWidget, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, 
-  QFileDialog, QInputDialog, QListWidget, QMessageBox, QLabel, 
-  QTextEdit, QApplication
+  QInputDialog, QListWidget, QMessageBox, QLabel, QTextEdit, QApplication
 )
+
 
 class GUI(QMainWindow):
 
@@ -41,31 +43,37 @@ class StartWindow(GUI):
     self.main_layout.addWidget(self.logo)
 
     self.report_button = QPushButton("1. Make a new report")
+    self.report_button.setToolTip("1")
     self.report_button.setShortcut('1')
     self.report_button.clicked.connect(self.report_clicked)
     self.main_layout.addWidget(self.report_button)
 
     self.todo_button = QPushButton("2. TODO list")
+    self.todo_button.setToolTip("2")
     self.todo_button.setShortcut('2')
     self.todo_button.clicked.connect(self.TODO_clicked)
     self.main_layout.addWidget(self.todo_button)
 
     self.specs_button = QPushButton("3. Ask specifications")
+    self.specs_button.setToolTip("3")
     self.specs_button.setShortcut('3')
     self.specs_button.clicked.connect(self.specs_clicked)
     self.main_layout.addWidget(self.specs_button)
 
     self.advice_button = QPushButton("4. Ask general advice")
+    self.advice_button.setToolTip("4")
     self.advice_button.setShortcut('4')
     self.advice_button.clicked.connect(self.advice_clicked)
     self.main_layout.addWidget(self.advice_button)
 
     self.edit_button = QPushButton("5. Edit report")
+    self.edit_button.setToolTip("5")
     self.edit_button.setShortcut('5')
     self.edit_button.clicked.connect(self.edit_clicked)
     self.main_layout.addWidget(self.edit_button)
 
     self.exit_button = QPushButton("6. Exit")
+    self.exit_button.setToolTip("6")
     self.exit_button.setShortcut('6')
     self.exit_button.clicked.connect(self.exit_clicked)
     self.main_layout.addWidget(self.exit_button)
@@ -91,12 +99,16 @@ class StartWindow(GUI):
       self.TODO = None
 
   def specs_clicked(self):
-    #TODO input dialog
-    pass
+    text, ok = QInputDialog.getText(self, "Spesifications", "What spesifications are you looking for?")
+    if ok and text != "":
+      #TODO pass text to AI. Pass AI reponse as class InformationWindow info parameter
+      pass
 
   def advice_clicked(self):
-    #TODO input dialog
-    pass
+    text, ok = QInputDialog.getText(self, "Advice", "What advice do you need?")
+    if ok and text != "":
+      #TODO pass to text AI. Pass AI reponse as class InformationWindow info parameter
+      pass
 
   def edit_clicked(self):
     if self.edit is None:
@@ -125,11 +137,10 @@ class ReportWindow(GUI):
     super().__init__()
     self.setWindowTitle("Report")
 
-    self.button_layout = QHBoxLayout()
-
     self.text_edit = QTextEdit(self)
     self.main_layout.addWidget(self.text_edit)
 
+    self.button_layout = QHBoxLayout()
     self.main_layout.addLayout(self.button_layout)
 
     self.cancel_button = QPushButton("Cancel")
@@ -156,7 +167,9 @@ class ReportWindow(GUI):
     self.start.show()
 
   def image_clicked(self):
-    pass
+    self.hide()
+    take_picture()
+    self.show()
 
   def submit_clicked(self):
     pass
@@ -186,18 +199,58 @@ class TODOWindow(GUI):
     pass
 
 
-class SpecsWindow(GUI):
-  pass
+class InformationWindow(GUI):
+  
+  def __init__(self, info):
+    super().__init__()
+    self.setWindowTitle("Information you asked for")
 
+    self.answer = QLabel()
+    self.answer.setText(info)
 
+    self.ok_button = QPushButton
+    self.ok_button.setToolTip("Enter")
+    self.ok_button.setShortcut('Enter')
+    self.ok_button.clicked.connect(self.ok_clicked)
+    self.main_layout.addWidget(self.ok_button)
 
-
-class AdviceWindow(GUI):
-  pass
+  def ok_clicked(self):
+    self.start = StartWindow()
+    self.close()
+    self.start.show()
 
 
 class EditWindow(GUI):
-  pass
+  
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle("Edit report")
+
+    self.text_edit = QTextEdit()
+    self.main_layout.addWidget(self.text_edit)
+
+    self.button_layout = QHBoxLayout()
+    self.main_layout.addLayout(self.button_layout)
+
+    self.cancel_button = QPushButton("Cancel")
+    self.cancel_button.setToolTip("Esc")
+    self.cancel_button.setShortcut('Esc')
+    self.cancel_button.clicked.connect(self.cancel_clicked)
+    self.button_layout.addWidget(self.cancel_button)
+
+    self.submit_button = QPushButton("Submit")
+    self.submit_button.setToolTip("Enter")
+    self.submit_button.setShortcut('Enter')
+    self.submit_button.clicked.connect(self.submit_clicked)
+    self.button_layout.addWidget(self.submit_button)
+
+  def cancel_clicked(self):
+    self.start = StartWindow()
+    self.close()
+    self.start.show()
+
+  def submit_clicked(self):
+    pass
 
 
 global app
