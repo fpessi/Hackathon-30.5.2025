4d22cb4a16a8a78fab1c38437c1869c017fc049c
fpessi <rolfpessi@gmail.com>
2025-05-31 21:48:30 +0300

    Started coding gui. Start window and report window initialized.

--- code/GUI.py
+++ code/GUI.py
@@ -1,6 +1,164 @@
-# For UI
+import sys
+import os
 
-class UI(object):
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import (
+  QWidget, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, 
+  QFileDialog, QInputDialog, QListWidget, QMessageBox, QLabel, 
+  QTextEdit, QApplication
+)
+
+class GUI(QMainWindow):
 
   def __init__(self):
+    super().__init__()
+    self.setGeometry(0, 0, 720, 405)
+    self.statusBar().setStatusTip("This is a statusbar")
+    self.main_widget = QWidget()
+    self.main_layout = QVBoxLayout()
+    self.main_widget.setLayout(self.main_layout)
+    self.setCentralWidget(self.main_widget)
+
+
+class StartWindow(GUI):
+
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle("Work Buddy")
+
+    self.report = None
+
+    self.logo = QLabel(self)
+    filepath = self.get_image_filepath()
+    self.logo.setPixmap(QPixmap(filepath))
+    self.main_layout.addWidget(self.logo)
+
+    self.report_button = QPushButton("1. Make a new report")
+    self.report_button.setShortcut('1')
+    self.report_button.clicked.connect(self.report_clicked)
+    self.main_layout.addWidget(self.report_button)
+
+    self.todo_button = QPushButton("2. TODO list")
+    self.todo_button.setShortcut('2')
+    self.todo_button.clicked.connect(self.TODO_clicked)
+    self.main_layout.addWidget(self.todo_button)
+
+    self.specs_button = QPushButton("3. Ask specifications")
+    self.specs_button.setShortcut('3')
+    self.specs_button.clicked.connect(self.specs_clicked)
+    self.main_layout.addWidget(self.specs_button)
+
+    self.advice_button = QPushButton("4. Ask advice")
+    self.advice_button.setShortcut('4')
+    self.advice_button.clicked.connect(self.advice_clicked)
+    self.main_layout.addWidget(self.advice_button)
+
+    self.edit_button = QPushButton("5. Edit report")
+    self.edit_button.setShortcut('5')
+    self.edit_button.clicked.connect(self.edit_clicked)
+    self.main_layout.addWidget(self.edit_button)
+
+    self.exit_button = QPushButton("6. Exit")
+    self.exit_button.setShortcut('6')
+    self.exit_button.clicked.connect(self.exit_clicked)
+    self.main_layout.addWidget(self.exit_button)
+
+    self.show()
+
+  def report_clicked(self):
+    if self.report is None:
+      self.report = ReportWindow()
+      self.close()
+      self.report.show()
+    else:
+      self.report.close()
+      self.report = None
+
+  def TODO_clicked(self):
     pass
+
+  def specs_clicked(self):
+    pass
+
+  def advice_clicked(self):
+    pass
+
+  def edit_clicked(self):
+    pass
+
+  def exit_clicked(self):
+    ok = QMessageBox.question(self, "Exit", "Are you sure?")
+    if ok == QMessageBox.StandardButton.Yes:
+      sys.exit()
+
+  def get_image_filepath(self):
+    absolute_path = os.path.dirname(__file__)
+    relative_path = r"../assets/pictures/logo.png"
+    filepath = os.path.join(absolute_path, relative_path)
+    return filepath
+
+
+class ReportWindow(GUI):
+  
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle("Report")
+
+    self.button_layout = QHBoxLayout()
+
+    self.text_edit = QTextEdit(self)
+    self.main_layout.addWidget(self.text_edit)
+
+    self.main_layout.addLayout(self.button_layout)
+
+    self.cancel_button = QPushButton("Cancel")
+    self.cancel_button.setToolTip("Esc")
+    self.cancel_button.setShortcut('Esc')
+    self.cancel_button.clicked.connect(self.cancel_clicked)
+    self.button_layout.addWidget(self.cancel_button)
+
+    self.image_button = QPushButton("Add Image")
+    self.image_button.setToolTip("I")
+    self.image_button.setShortcut('I')
+    self.image_button.clicked.connect(self.image_clicked)
+    self.button_layout.addWidget(self.image_button)
+
+    self.submit_button = QPushButton("Submit")
+    self.submit_button.setToolTip("Enter")
+    self.submit_button.setShortcut('Enter')
+    self.submit_button.clicked.connect(self.submit_clicked)
+    self.button_layout.addWidget(self.submit_button)
+
+  def cancel_clicked(self):
+    self.start = StartWindow()
+    self.close()
+    self.start.show()
+
+  def image_clicked(self):
+    pass
+
+  def submit_clicked(self):
+    pass
+
+
+class TODOWindow(QWidget):
+  pass
+
+
+class SpecsWindow(QWidget):
+  pass
+
+
+class AdviceWindow(QWidget):
+  pass
+
+
+class EditWindow(QWidget):
+  pass
+
+
+global app
+app = QApplication(sys.argv)
+start = StartWindow()
+sys.exit(app.exec())
