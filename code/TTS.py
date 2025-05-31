03b22e1f46be1a5d376b4cfc839b5b932c173c20
fpessi <rolfpessi@gmail.com>
2025-05-31 14:05:34 +0300

    Added basic classes

---
+++ code/TTS.py
@@ -0,0 +1,9 @@
+class TextToSpeech(object):
+
+  def __init__(self):
+    pass
+
+class SpeechToText(object):
+
+  def __init__(self):
+    pass
---
+++ code/report_compiler.py
@@ -0,0 +1,7 @@
+# This object compiles a docx format report from user input
+# Uses python-docx library
+
+class Reporter(object):
+
+  def __init__(self):
+    pass
