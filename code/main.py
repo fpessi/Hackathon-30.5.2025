9cf8bd6e1bb215626563c43baf9c88489601d759
fpessi <rolfpessi@gmail.com>
2025-05-31 19:27:35 +0300

    Improved main readability

--- code/main.py
+++ code/main.py
@@ -1,19 +1,23 @@
 from ai_communication import request
+
+
 def main():
     print("Write your questions. Stop by writing exit.")
-    x=input() #user writes their input for the ai
-    while x.lower()!="exit":
-        if isinstance(x,str):
-            result=request(x) #the users text is sent to the ai to process
-            if result==None:
+    x = input() #user writes their input for the ai
+
+    while x.lower() != "exit":
+        if isinstance(x, str):
+            result = request(x) #the users text is sent to the ai to process
+            if result == None:
                 print("FAILURE: try again")
             else:
                 edited_result=result["choices"][0]["text"]
                 print(edited_result)
-            x=input()
+            x = input()
         else:
             print("FAILURE: You need to write a string") #if the user somehow gives other value that string
             x = input()
+            
     print("Exiting software")
 
 main()
